1e82abeb97718590fd36e05c1d6ff76f56d86f66
nishio1993 <grace240@docomo.ne.jp>
2023-01-10 03:52:40 +0900

    引数を出力できるようにした

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿var output = new List<String>();
+﻿var arg = args[0];
+
+var output = new List<String>();
 output.Add(".intel_syntax noprefix");
 output.Add(".global main");
 output.Add("");
 output.Add("main:");
-output.Add("    mov rax, 42");
+output.Add($"    mov rax, {arg}");
 output.Add("    ret");
 output.Add("");
 File.WriteAllText("./Program.s", string.Join("\n", output));
