49499587d9e97faadf5271789d827886218f9ae6
nishio1993 <grace240@docomo.ne.jp>
2023-01-11 23:22:03 +0900

    テスト改良

--- test.sh
+++ test.sh
@@ -4,6 +4,10 @@ test() {
 	expected="$2"
 
     dotnet run "$input"
+    build="$?"
+    if [ "$build" = 1 ]; then
+        exit 1
+    fi
     gcc -o test test.s
     ./test
     result="$?"
@@ -17,3 +21,5 @@ test() {
 
 test "1+2" 3
 test "1+23+45" 69
+test "6-4" 2
+test "123-45-6" 72
